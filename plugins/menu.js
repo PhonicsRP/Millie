24428b6a24742b6a05e2cba03dc02b8a560032f2
PhonicsRP <92306989+PhonicsRP@users.noreply.github.com>
2021-10-11 23:06:26 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/images5/Original-Cool-Stranger-Things-Wallpaper.jpg";
+    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
     
     
     var i = Math.floor(1*Math.random())
